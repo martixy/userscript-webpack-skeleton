6efbc33b510b993993f0897c5cb29ecfdc172afe
martixy <martin.ninov@gmail.com>
2018-09-05 10:10:22 +0300

    Untrack dist, the whole point here was the build process.

--- dist/local.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         userscript-webpack-skeleton
-// @version      0.1.0
-// @description  A skeleton for userscript development utilizing webpack
-// @author       martixy
-// @grant        GM_info
-// @match        https://www.google.com/
-// @require      file:///C:/Users/martixy/Desktop/Scripts/userscript-webpack-skeleton/dist/main.js
-// ==/UserScript==
